11f0ee810966dadcbf39b71fc722eae8b88c42e5
Jeff Sheltren <jeff@tag1consulting.com>
2014-02-27 14:32:26 -0800

    Missing commas in drush yumrepo definitions

--- manifests/drush5.pp
+++ manifests/drush5.pp
@@ -2,7 +2,7 @@ class yumrepos::drush5 {
 
   yumrepo { 'tag1-drush5':
     descr      => 'Tag1 Drush 5',
-    baseurl => "http://pkg.tag1consulting.com/drush/drush-5/el${::operatingsystemmajrelease}/noarch/"
+    baseurl => "http://pkg.tag1consulting.com/drush/drush-5/el${::operatingsystemmajrelease}/noarch/",
     enabled   => '1',
     gpgcheck   => '0',
   }
